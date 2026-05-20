41ab09014bdefaacaaef7b0fd4d38a31384b09fe
teacher-kev <kevin.hernandez@adoc.superate.org.sv>
2026-05-20 16:18:09 -0600

    Primer Commit

---
+++ php/conexion.php
@@ -0,0 +1,15 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "db_dontono";
+    $conn = new mysqli($host, $user, $password,$dbname);
+
+    if ($conn->connect_error) {
+        die("Error de Conexión". $conn->connect_error);
+    }
+
+    $conn->set_charset("utf8mb4");
+
+?>
+
